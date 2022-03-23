8f6d01b8feb6a387e3560f0e27be298173700e3c
Sharar Nur <shararnur@gmail.com>
2022-03-23 08:28:51 +0600

    first commit

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './Login.css';
-import img from '../../resource/Group 312.png';
 import logo from '../../resource/blue-logo 1.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook, faGoogle } from '@fortawesome/free-brands-svg-icons';
@@ -9,37 +8,44 @@ import { faFacebook, faGoogle } from '@fortawesome/free-brands-svg-icons';
 
 const Login = () => {
     return (
-        <div className="main">
-            <div className="side-part">
-                <img src={img} alt="" className="bg-img" />
+        <div className="main d-flex">
+            <div className="left-side">
+                <div className="bg-img">
+                    {/* Added background image */}
+                </div>
             </div>
-            <div className="login-panel">
-                <div className="login-form text-center">
-                    <div>
-                        <img src={logo} alt="" className="logo" />
+
+            <div className="container">
+                <div className="right-side">
+                    <div className="login-box">
+                        <div className="inner-box">
+                            <div className="top-part text-center">
+                                <img src={logo} alt="" className="logo mb-3 mt-5" />
+                            </div>
+                            <h4 className="heading text-center">Explore The Best Tours... Hurry up!!</h4>
+                            <form className="custom-width">
+                                <div className="mb-3">
+                                    <input type="email" className="form-control cst-border" placeholder="Email Address" />
+                                </div>
+                                <div className="mb-3">
+                                    <input type="password" className="form-control cst-border" placeholder="Password" />
+                                </div>
+
+                                <a href="#" className="forget-pass mb-3">Forget Password?</a>
+
+                                <button type="submit" className="btn btn-primary login-btn fw-bold mb-4">LOG IN</button>
+
+                                <p className="txt-or fw-bold mb-5"><span className="alternate">or</span></p>
+
+                                <div className="icon mb-4">
+                                    <a href="#" className="fb-icon"><FontAwesomeIcon icon={faFacebook} size="2x" /></a>
+                                    <a href="#" className="google-icon"><FontAwesomeIcon icon={faGoogle} size="2x" /></a>
+                                </div>
+
+                                <p className="text-muted text-center">Don't have an account? <a href="#" className="sign-up fw-bold">Sign Up</a> </p>
+                            </form>
+                        </div>
                     </div>
-                    <h4 className=" heading text-center">Explore The Best Tours... Hurry up!!</h4>
-                    <form>
-                        <div className="mb-3">
-
-                            <input type="email" className="form-control" id="exampleInputEmail1" placeholder="Email Address" />
-                        </div>
-                        <div className="mb-3">
-                            <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
-                        </div>
-                        <p className="txt-red" style={{ fontWeight: '400' }}>Forgot Password?</p>
-                        <button type="submit" className="btn btn-primary loginbtn fw-bold">LOG IN</button>
-
-                        <div className="optional mt-5 fw-bold">
-                            <p className="or"><span>or</span></p>
-                        </div>
-
-                        <div className="mt-4">
-                            <a href="" className="me-5"><FontAwesomeIcon icon={faFacebook} size="2x" style={{ color: '#4267B2 ' }} /></a>
-                            <a href=""><FontAwesomeIcon icon={faGoogle} size="2x" style={{ color: '#4285F4 ' }} /></a>
-                        </div>
-                        <p className="mt-4">Don't have an account? <a href="" className="signup">Sign Up</a> </p>
-                    </form>
                 </div>
             </div>
         </div>
